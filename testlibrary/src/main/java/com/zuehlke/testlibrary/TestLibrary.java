f7514e9c69982a9c69ea52c9fd2b9f17a9b65491
Diegas, Stefan <stefan.diegas@zuehlke.com>
2019-01-15 12:55:10 +0100

    Create project in Studio. Create TestLibrary for easy debugging

---
+++ testlibrary/src/main/java/com/zuehlke/testlibrary/TestLibrary.java
@@ -0,0 +1,13 @@
+package com.zuehlke.testlibrary;
+
+import android.util.Log;
+
+public class TestLibrary {
+
+    private static final String TAG = "SUPER_AWESOME_APP";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+
+}
